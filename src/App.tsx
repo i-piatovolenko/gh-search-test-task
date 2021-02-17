7a75c08153b069045f13c06b3869a710ce18ee26
vano1208 <120894v@gmail.com>
2021-02-17 18:33:39 +0200

    apollo client added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from "./pages/main/Main";
+
+const App = () => {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../../components/header/Header";
+import Content from "../../components/content/Content";
+
+const Main = () => {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+    </div>
+  );
+}
+
+export default Main;
